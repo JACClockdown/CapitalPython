0d349bfa23591347888f2bb6302b85da23763d03
Beto <wortev210@gmail.com>
2023-12-13 18:04:42 -0600

    feature: deploy redis

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ from clases.firmamex import FirmamexServices
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 import json
+import redis
 
 app = FastAPI()
 
 webid = "LspRxpD0ihLFwx7Y"
 apikey = "17cbc02700b843e741e00a851dec67e4"
+
+
 
 class Item(BaseModel):
     ticket  : str
@@ -33,11 +36,32 @@ def chek_workflow( items: List[Item] ):
 
 @app.post("/items")
 def read_item( items: List[Item] ):
-    
+
     services = FirmamexServices(webid, apikey)
     response = services.getReport(items[0].ticket)
     json_object = json.loads(response)
-    return { "list": json_object}
+    loop_firmas = []
+    
+    for i, list_item in enumerate(json_object['firmas']):
+        loop_firmas.append({
+            "signDate" : list_item['signDate'],
+            "email"    : list_item['certificados'][0]['email']
+        })
+    
+    response = {
+        "firmas" : loop_firmas,
+        "document" : json_object['document'],
+        "documentStatus" : json_object['documentStatus'],
+        "ticket" :json_object['ticket'],
+        "originalName" : json_object['originalName'],
+        "pendingSigners" : json_object['pendingSigners'],
+    }
+    
+    r = redis.Redis(host='redis-cache', port=6379, db=0)
+    r.set('firma',json.dumps(response))
+    
+    return response
+
 
 @app.post("/document")
 def read_item( items: List[Item] ):
